0b47776c5cca83a3640a8ba69f76c692762c5cf9
DToxv2 <joaoaraujo.78@gmail.com>
2020-07-08 11:10:09 +0100

    1.0.0.1

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,1 +1,23 @@
-print('Test')
+import pygame, sys
+
+def run_game():
+    pygame.init()
+    screen = pygame.display.set_mode((1200,800))
+    pygame.display.set_caption('Alien Invasion')
+    bg_color = (230, 230, 230)
+
+    # Main Game Loop
+    while True:
+
+        # Keyboard and mouse events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        # Redraw the screen
+        screen.fill(bg_color)
+
+        # Make the most recently drawn screen visible
+        pygame.display.flip()
+
+run_game()
