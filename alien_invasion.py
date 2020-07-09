00ce995823acaff4de9c0e6d3b8a6493bbb8c380
DToxv2 <joaoaraujo.78@gmail.com>
2020-07-09 15:03:24 +0100

    1.0.1

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import pygame, sys
 
 from settings import Settings
 from ship import Ship
+from pygame.sprite import Group
 import game_functions as gf
 
 def run_game():
@@ -13,11 +14,14 @@ def run_game():
 
     # Make a ship
     ship = Ship(ai_settings, screen)
+    bullets = Group()
 
     # Main Game Loop
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
+        pygame.display.flip()
 
 run_game()
--- settings.py
+++ settings.py
@@ -1,10 +1,17 @@
 class Settings():
     def __init__(self):
 
+        # Bullet Settings
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3
+
         # Screen Settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (0, 0, 0)
 
         # Ship Settings
         self.ship_speed_factor = 1.25
